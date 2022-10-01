a1d43df31e64a17891f26dfeaf7c3fd684f0ef1c
Britt Carlton <brittanyrc95@gmail.com>
2022-10-01 15:20:18 -0400

    Create work page structure

---
+++ src/pages/work.js
@@ -0,0 +1,11 @@
+import { ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
+
+const Work = () => {
+    return (
+        <ImageList sx={{ width: 450, height: 800 }}>
+            {/* map over list of projects to fill image list */}
+        </ImageList>
+    );
+}
+
+export default Work;
