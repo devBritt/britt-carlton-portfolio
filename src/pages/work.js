b047c66c73cdd2407759d10e6936f29b3698380d
Britt Carlton <brittanyrc95@gmail.com>
2022-10-04 20:40:00 -0400

    Reformat work page image list

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,20 +1,28 @@
-import { ImageList, ImageListItem } from '@mui/material';
-import { projectList } from '../assets/js/projectsList';
+import { ImageList, ImageListItem, Typography, Button } from '@mui/material';
+import { projectList } from '../utils/projectsList';
 
 const Work = () => {
     return (
         <ImageList
             cols={2}
-            gap={20}
+            gap={25}
+            sx={{width: 8/10, height: 8/10}}
         >
             {projectList.map((project, index) => (
                 <ImageListItem key={index}>
-                    <img
-                        src={`/images/${project.img}`}
-                        alt={`${project.name} thumbnail`}
-                        loading={'lazy'}
-                        // TODO: add onClick to open modal with info
-                    />
+                    <Button>
+                        <img
+                            src={`/assets/images/${project.img}`}
+                            alt={`${project.name} thumbnail`}
+                            loading={'lazy'}
+                        />
+                        <Typography
+                            component={'h2'}
+                            variant={'h3'}
+                        >
+                            {project.name}
+                        </Typography>
+                    </Button>
                 </ImageListItem>
             ))}
         </ImageList>
