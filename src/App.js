88c2a55242878c60b092d1ac4ea43350e40752e8
Britt Carlton <brittanyrc95@gmail.com>
2022-10-05 11:31:57 -0400

    Override MUI color palette with custom colors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './assets/css/App.css';
 import { CssBaseline, Container } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, hslToRgb, ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // fonts
@@ -17,6 +17,59 @@ import Contact from './pages/contact';
 import Resume from './pages/resume';
 
 const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        common: {
+            black: '#1c2535',
+            white: '#f7f7f8'
+        },
+        primary: {
+            main: '#6be6f5',
+            light: '#a7f2fa',
+            dark: '#0cbad6'
+        },
+        secondary: {
+            main: '#ddbaf8',
+            light: '#ecd9fb',
+            dark: '#b363e9'
+        },
+        error: {
+            main: '#fdb5a4',
+            light: '#fed4ca',
+            dark: '#fed4ca'
+        },
+        warning: {
+            main: '#efc44b',
+            light: '#f5de92',
+            dark: '#e5941b'
+        },
+        info: {
+            main: '#f3b4eb',
+            light: '#f9d5f6',
+            dark: '#de5fcc'
+        },
+        success: {
+            main: '#91e4ab',
+            light: '#c1f1d0',
+            dark: '#33b45c'
+        },
+        grey: {
+            50: '#f4f5f6',
+            100: '#ebedef',
+            200: '#dfe3e7',
+            300: '#b1bdc8',
+            400: '#8a99a8',
+            500: '#697a8c',
+            600: '#556372',
+            700: '#404c59',
+            800: '#323c48',
+            900: '#232a34',
+            A100: '#ebedef',
+            A200: '#dfe3e7',
+            A400: '#8a99a8',
+            A700: '#404c59'
+        }
+    },
     typography: {
         fontFamily: [
             'Mersad',
@@ -24,24 +77,12 @@ const theme = createTheme({
             'Space Mono Bold Italic',
             'Make Sans'
         ],
-        h1: {
-            fontFamily: 'Mersad'
-        },
-        h2: {
-            fontFamily: 'Mersad'
-        },
-        h3: {
-            fontFamily: 'Space Mono Bold'
-        },
-        h4: {
-            fontFamily: 'Space Mono Bold'
-        },
-        h5: {
-            fontFamily: 'Space Mono Bold'
-        },
-        h6: {
-            fontFamily: 'Space Mono Bold'
-        },
+        h1: { fontFamily: 'Mersad'},
+        h2: { fontFamily: 'Mersad' },
+        h3: {fontFamily: 'Space Mono Bold' },
+        h4: { fontFamily: 'Space Mono Bold' },
+        h5: { fontFamily: 'Space Mono Bold' },
+        h6: { fontFamily: 'Space Mono Bold' },
         subtitle1: {
             fontFamily: 'Space Mono Bold Italic',
             fontSize: '1.25rem'
